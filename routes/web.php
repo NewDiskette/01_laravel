e3e1b427c79ead778c7017be861f82d65a4b2fce
newdiskette <newdiskette@gmail.com>
2022-06-30 11:27:47 +0500

    change routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my_page', function () {
+    return '<h1>This is my page.</h1>';
+});
+
+Route::get('/page_centered', function () {
+    return '<h1 style="text-align: center">This page is centered.</h1>';
+});
+
+Route::get('/my_hobby', function () {
+    return '<h1>My hobby is development.</h1>';
+});
+
+Route::get('/my_language', function () {
+    return '<h1>My favorite language of development is PHP.</h1>';
+});
