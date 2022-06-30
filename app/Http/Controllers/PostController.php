232b4db792df995adf074fcd22e7391676bc41c2
newdiskette <newdiskette@gmail.com>
2022-06-30 14:16:34 +0500

    changed PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
-
+        $post = Post::find(1);
+        dd($post);
     }
 }
