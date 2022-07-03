6ac5ef48f1d63e489595f5aeea59f1f4da512b70
newdiskette <newdiskette@gmail.com>
2022-07-03 09:52:00 +0500

    requared bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,21 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 
-    <div>
-        <nav>
-            <ul>
-              <li><a href="{{ route('main.index') }}">Main</a></li>
-              <li><a href="{{ route('post.index') }}">Posts</a></li>
-              <li><a href="{{ route('about.index') }}">About</a></li>
-              <li><a href="{{ route('contact.index') }}">Contacts</a></li>
-            </ul>
-        </nav>
-    </div>
+    <div class="container">
 
-    @yield('content')
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('main.index') }}">Main</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('about.index') }}">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+        @yield('content')
+
+    </div>
 
 </body>
 </html>
